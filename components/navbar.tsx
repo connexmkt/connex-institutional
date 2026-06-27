945c002b5d5e08b33e3c01be5318148bf4763a96
luuizfernando <luizfernandosant26@gmail.com>
2026-06-27 20:49:31 -0300

    disparar deploy

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -29,8 +29,9 @@ export function Navbar() {
       initial={{ y: -100 }}
       animate={{ y: 0 }}
       transition={{ duration: 0.5 }}
-      className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${isScrolled ? "border-b border-border/20" : ""
-        }`}
+      className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${
+        isScrolled ? "border-b border-border/20" : ""
+      }`}
       style={{ backgroundColor: isScrolled ? "#161622" : "transparent" }}
     >
       <nav className="container mx-auto px-4 md:px-6 h-20 flex items-center justify-between relative">
